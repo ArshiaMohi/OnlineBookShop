a8b4c123b13fb41064d9a2bfbb8238454356f987
ArshiaMohi <arshiamohi675@gmail.com>
2026-06-27 17:33:29 +0330

    integrate Swagger UI documentation

--- src/main/java/com/example/project/controller/BookController.java
+++ src/main/java/com/example/project/controller/BookController.java
@@ -3,11 +3,14 @@ package com.example.project.controller;
 import com.example.project.dto.BookSaveDto;
 import com.example.project.model.Book;
 import com.example.project.service.BookService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "Books", description = "Book Management APIs")
 @RestController
 @RequestMapping("/books")
 public class BookController {
@@ -18,16 +21,19 @@ public class BookController {
         this.bookService = bookService;
     }
 
+    @Operation(summary = "Get book with id")
     @GetMapping("/{id}")
     public Book findById(@PathVariable int id) {
         return bookService.findById(id);
     }
 
+    @Operation(summary = "Get all books")
     @GetMapping()
     public List<Book> findAll() {
         return bookService.findAll();
     }
 
+    @Operation(summary = "Update a book")
     @PostMapping("/update")
     public Book update(@Valid @RequestBody BookSaveDto bookSaveDto) {
         Book book = bookSaveDto.convert();
@@ -35,6 +41,7 @@ public class BookController {
         return book;
     }
 
+    @Operation(summary = "Delete a book")
     @GetMapping("/delete/{id}")
     public void delete(@PathVariable int id) {
         Book book = bookService.findById(id);
