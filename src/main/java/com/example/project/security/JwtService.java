8923d841a002525091593a20bef412620a391e57
ArshiaMohi <arshiamohi675@gmail.com>
2026-06-29 18:53:24 +0330

    feat: prepare JWT service

--- src/main/java/com/example/project/security/JwtService.java
+++ src/main/java/com/example/project/security/JwtService.java
@@ -1,7 +1,24 @@
 package com.example.project.security;
 
+import com.example.project.model.ApplicationUser;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JwtService {
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    public String getSecretKey() {
+        return secretKey;
+    }
+
+    public void setSecretKey(String secretKey) {
+        this.secretKey = secretKey;
+    }
+
+    public String generateToken(ApplicationUser user){
+        
+    }
 }
